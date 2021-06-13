3cdbfe28f08a0f5c24d22e478b69a62240a5108b
Dan nguyen <danek.nguyen@gmail.com>
2021-06-13 16:46:25 +0200

    Přidejte soubory projektu.

---
+++ Budgeter/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeter
+{
+    public class Item
+    {
+        public static List<Item> items = new List<Item>();
+
+        public string name;
+        public int price;
+        public DateTime? dateOfPurchase;
+
+        public Item()
+        { }
+
+        public Item(string Name, string Price, DateTime? date)
+        {
+            name = Name;
+            price = Convert.ToInt32(Price);
+            dateOfPurchase = date;
+        }
+    }
+}
